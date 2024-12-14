dc698f628922aa058635a9c5ee9103a546c78706
Felipe.Macedo <felipealexandrej@gmail.com>
2024-12-13 21:51:38 -0300

    style(categories): enhance table and form UI - Improved table hover effects and button styles - Enhanced error messages for invalid inputs

--- src/models/categorias/Categoria.ts
+++ src/models/categorias/Categoria.ts
@@ -1,4 +1,9 @@
 export default interface Categoria {
-    id: number;
-    tipo: string;
-}
+    id?: number;
+    tipo: string; // Nome ou tipo da categoria, como "RPG", "Ação", etc.
+    descricao?: string; // Uma breve descrição da categoria.
+    icone?: string; // URL ou referência ao ícone da categoria.
+    ativo: boolean; // Indica se a categoria está ativa ou não.
+    dataCriacao?: Date; // Data de criação da categoria.
+  }
+  
